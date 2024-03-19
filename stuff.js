e50ed8c9d90ee2f56f366beca1b2e51d8bbb8a1f
kianamay <92769721+kianamay@users.noreply.github.com>
2024-03-19 00:46:39 +0000

    api key

--- stuff.js
+++ stuff.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://mzrrfxjkxestlzclvjvs.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16cnJmeGpreGVzdGx6Y2x2anZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTY1MzYsImV4cCI6MjAyNjM3MjUzNn0.TS3XyLGOYmluoDbwJCC5HYMZ4vy_i_r2DVYeIm4hSBk'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function getBooks(){
