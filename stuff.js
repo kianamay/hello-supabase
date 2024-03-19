89346fa40b08920bd7bc52fada04ae67ced3c2fd
kianamay <92769721+kianamay@users.noreply.github.com>
2024-03-19 01:43:30 +0000

    update

--- stuff.js
+++ stuff.js
@@ -1,5 +1,5 @@
 
-const { createClient } = supabase
+import { createClient } from '@supabase/supabase-js'
 //const supabase = createClient('https://mzrrfxjkxestlzclvjvs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16cnJmeGpreGVzdGx6Y2x2anZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTY1MzYsImV4cCI6MjAyNjM3MjUzNn0.TS3XyLGOYmluoDbwJCC5HYMZ4vy_i_r2DVYeIm4hSBk')
 const supabaseUrl = 'https://mzrrfxjkxestlzclvjvs.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
