1ac768cbab530ec84d0a6d17178d0bfa9b094013
kianamay <92769721+kianamay@users.noreply.github.com>
2024-03-19 01:23:44 +0000

    install supa

--- stuff.js
+++ stuff.js
@@ -1,8 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://mzrrfxjkxestlzclvjvs.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16cnJmeGpreGVzdGx6Y2x2anZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTY1MzYsImV4cCI6MjAyNjM3MjUzNn0.TS3XyLGOYmluoDbwJCC5HYMZ4vy_i_r2DVYeIm4hSBk'
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient('https://mzrrfxjkxestlzclvjvs.supabase.co'
+, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16cnJmeGpreGVzdGx6Y2x2anZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTY1MzYsImV4cCI6MjAyNjM3MjUzNn0.TS3XyLGOYmluoDbwJCC5HYMZ4vy_i_r2DVYeIm4hSBk')
 
 async function getBooks(){
   let { data: books, error } = await supabase
