21fd71abaf293cf90bf37b24b24fe5425c48691d
kianamay <92769721+kianamay@users.noreply.github.com>
2024-03-19 01:39:08 +0000

    first few lines

--- stuff.js
+++ stuff.js
@@ -1,6 +1,9 @@
 
-import { createClient } from '@supabase/supabase-js'
-const supabase = createClient('https://mzrrfxjkxestlzclvjvs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16cnJmeGpreGVzdGx6Y2x2anZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTY1MzYsImV4cCI6MjAyNjM3MjUzNn0.TS3XyLGOYmluoDbwJCC5HYMZ4vy_i_r2DVYeIm4hSBk')
+const { createClient } = supabase
+//const supabase = createClient('https://mzrrfxjkxestlzclvjvs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16cnJmeGpreGVzdGx6Y2x2anZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTY1MzYsImV4cCI6MjAyNjM3MjUzNn0.TS3XyLGOYmluoDbwJCC5HYMZ4vy_i_r2DVYeIm4hSBk')
+const supabaseUrl = 'https://mzrrfxjkxestlzclvjvs.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function getBooks(){
   let { data: books, error } = await supabase
