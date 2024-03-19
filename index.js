518ce0a6d3eff3f693fa2ccc82379da95b2d1923
kianamay <92769721+kianamay@users.noreply.github.com>
2024-03-19 00:35:43 +0000

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js"
+
+import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://mzrrfxjkxestlzclvjvs.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16cnJmeGpreGVzdGx6Y2x2anZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTY1MzYsImV4cCI6MjAyNjM3MjUzNn0.TS3XyLGOYmluoDbwJCC5HYMZ4vy_i_r2DVYeIm4hSBk'
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function getBooks(){
